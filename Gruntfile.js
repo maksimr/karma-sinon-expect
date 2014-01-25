67e2af64025b6542dd7a63a127c1a9f2290196d2
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-01-26 03:44:22 +0400

    Add release

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,12 +20,32 @@ module.exports = function(grunt) {
                 singleRun: true
             }
         },
+        bump: {
+            options: {
+                commitMessage: 'chore: release v%VERSION%',
+                pushTo: 'origin'
+            }
+        },
+        'npm-publish': {
+            options: {
+                requires: ['test'],
+                abortIfDirty: true
+            }
+        },
+        'npm-contributors': {
+            options: {
+                commitMessage: 'chore: update contributors'
+            }
+        },
         watch: {
             files: ['index.js', 'src/**/*.js', '<%= simplemocha.unit.src %>'],
             tasks: ['test']
         }
     });
 
+    grunt.registerTask('release', 'Bump the version and publish to NPM.', function(type) {
+        return grunt.task.run(['npm-contributors', "bump:" + (type || 'patch'), 'npm-publish']);
+    });
     grunt.registerTask('test', ['simplemocha:unit', 'karma:unit']);
     grunt.registerTask('default', ['test']);
 };
